2447e8d6628b6e87507d63a2040c2e8fc9f2387b
Swidnixx <swidnixx@gmail.com>
2023-09-30 11:40:09 +0200

    Ground Detection Mechanim

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,24 +5,66 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     //Settings
-    public float moveSpeed = 1;
-    public float rotSpeed = 100;
+    public float regularSpeed = 7;
+    public float fastSpeed = 14;
+    public float slowSpeed = 4;
+    public float rotSpeed = 250;
+    public LayerMask groundMask;
 
     //Dependencies
     public Transform cam;
     CharacterController controller;
 
     //Inner settings
+    float speed;
     float camRotX;
 
     private void Start()
     {
+        speed = regularSpeed;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         controller = GetComponent<CharacterController>();
     }
 
     private void Update()
+    {
+        Movement();
+        GroundDetection();
+    }
+
+    void GroundDetection()
+    {
+        RaycastHit hit;
+        float distance = controller.height * 0.5f - controller.radius;
+        bool didHit = Physics.SphereCast(transform.position, controller.radius, Vector3.down,
+            out hit, distance + 0.1f, groundMask);
+
+        if(didHit)
+        {
+            Debug.Log("Pod³o¿e: " + hit.collider.tag);
+            switch (hit.collider.tag)
+            {
+                case "Fast":
+                    speed = fastSpeed;
+                    break;
+
+                case "Slow":
+                    speed = slowSpeed;
+                    break;
+
+                default:
+                    speed = regularSpeed;
+                    break;
+            }
+        }
+        else
+        {
+            speed = regularSpeed;
+        }
+    }
+
+    void Movement()
     {
         //Collect Input
         float inputX = Input.GetAxis("Horizontal"); // lewo: -1; prawo: 1; nic: 0
@@ -32,7 +74,7 @@ public class PlayerController : MonoBehaviour
 
         //Movement
         Vector3 move = transform.right * inputX + transform.forward * inputY;
-        controller.Move(move * Time.deltaTime * moveSpeed);
+        controller.Move(move * Time.deltaTime * speed);
 
         //Rotation
         transform.Rotate(0, mouseX * Time.deltaTime * rotSpeed, 0);
@@ -44,5 +86,15 @@ public class PlayerController : MonoBehaviour
 
         //Gravity
         controller.Move(Physics.gravity * Time.deltaTime);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (controller == null) return;
+
+        Gizmos.color = Color.red;
+        float distance = controller.height * 0.5f - controller.radius;
+        Vector3 pos = transform.position + Vector3.down * (distance + 0.1f);
+        Gizmos.DrawSphere(pos, controller.radius);
     }
 }
